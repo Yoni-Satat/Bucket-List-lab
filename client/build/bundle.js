a7a97b1a0aa0a634445189e71f3e88c1a3a33535
Yoni Satat <yoni.satat@gmail.com>
2017-12-20 13:12:49 +0000

    added get, post and delete function to request.js

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -65,13 +65,76 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-let countries = [];
+const Request = __webpack_require__(1);
+
+const requestCountry = new Request("https://restcountries.eu/rest/v2/all");
 
 const app = function() {
-  const url = "https://restcountries.eu/rest/v2/all";
+  const select = document.querySelector('#countries');
+  requestCountry.get(function(countries) {
+    countries.forEach(function(country) {
+      const option = document.createElement('option');
+      option.innerText = country.name;
+      select.appendChild(option);
+    });
+  });
+
+  
 }
+
+document.addEventListener('DOMContentLoaded', app);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+const Request = function(url) {
+  this.url = url;
+}
+
+Request.prototype.get = function(callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', this.url);
+  request.addEventListener('load', function() {
+    if(this.status !== 200) {
+      return;
+    }
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  });
+  request.send();
+}
+
+Request.prototype.post = function(callback, body) {
+  const request = new XMLHttpRequest();
+  request.open('POST', this.url);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.addEventListener('load', function() {
+    if(this.status !== 201) {
+      return;
+    }
+    const responseBody = JSON.parse(this.responseText);
+    callback(responseBody);
+  });
+  request.send(JSON.stringify(body));
+}
+
+Request.prototype.delete = function(callback) {
+  const request = new XMLHttpRequest();
+  request.open('DELETE', this.url);
+  request.addEventListener('load', function() {
+    if(this.status !== 204) {
+      return;
+    }
+    callback();
+  });
+  request.send();
+}
+
+module.exports = Request;
 
 
 /***/ })
--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,18 @@
-let countries = [];
+const Request = require('./services/request.js');
+
+const requestCountry = new Request("https://restcountries.eu/rest/v2/all");
 
 const app = function() {
-  const url = "https://restcountries.eu/rest/v2/all";
+  const select = document.querySelector('#countries');
+  requestCountry.get(function(countries) {
+    countries.forEach(function(country) {
+      const option = document.createElement('option');
+      option.innerText = country.name;
+      select.appendChild(option);
+    });
+  });
+
+  
 }
+
+document.addEventListener('DOMContentLoaded', app);
