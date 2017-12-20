f391895c84432bc9e075738a693c6b0bbba479f3
Yoni Satat <yoni.satat@gmail.com>
2017-12-20 12:40:33 +0000

    initial commit

---
+++ client/src/app.js
@@ -0,0 +1,5 @@
+let countries = [];
+
+const app = function() {
+  const url = "https://restcountries.eu/rest/v2/all";
+}
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static('client/build'));
+
+
+server.listen(3000, function() {
+  console.log("Listening on port 3000...");
+});
